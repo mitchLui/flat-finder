551622f293b8735fcce9c27323fae6e483991b41
Mitch Lui <katinlui@gmail.com>
2021-01-21 15:33:03 +0000

    Removed unneeded libraries

--- accom_bot.py
+++ accom_bot.py
@@ -4,11 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 import re
 import unittest
 import json
